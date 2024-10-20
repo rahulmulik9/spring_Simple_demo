7951b3a5de011964a32359eb143b0b5960035fee
RAHUL <cycv@jhhj/com>
2024-10-20 22:43:21 +0530

    Filtering 1) Static Filtering 2) Dynamic Filtering

--- src/main/java/com/rahul/microservices/filtering/FilteringController.java
+++ src/main/java/com/rahul/microservices/filtering/FilteringController.java
@@ -1,14 +1,37 @@
 package com.rahul.microservices.filtering;
 
 
+import com.fasterxml.jackson.databind.ser.FilterProvider;
+import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
+import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
+import org.springframework.http.converter.json.MappingJacksonValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.List;
 
 @RestController
 public class FilteringController {
 
-    @GetMapping("/filter")
-    public SomeBean filtering(){
-        return new SomeBean("value1", "value2","value3");
+    //static filtering
+    @GetMapping("/filterStat")
+    public SomeBean filtering() {
+        return new SomeBean("value1", "value2", "value3");
+    }
+
+    //dynamic filtering
+    @GetMapping("filterDy")
+    public MappingJacksonValue filteringDyanmic() {
+        SomeBeanDy theBeam = new SomeBeanDy("value1", "value2", "value3");
+        //you can also use list
+        //List<SomeBean> list = Arrays.asList(new SomeBean("value1", "value2", "value3"), new SomeBean("value4", "value5", "value6"));
+        //MappingJacksonValue mappingJacksonValue = new MappingJacksonValue(list);
+        MappingJacksonValue mappingJacksonValue = new MappingJacksonValue(theBeam);
+        SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter.filterOutAllExcept("field1","field3");
+        FilterProvider theProvider = new SimpleFilterProvider().addFilter("SomeBeanFilter",filter);
+        mappingJacksonValue.setFilters(theProvider);
+        return mappingJacksonValue;
     }
 }
---
+++ src/main/java/com/rahul/microservices/filtering/SomeBeanDy.java
@@ -0,0 +1,15 @@
+package com.rahul.microservices.filtering;
+
+
+public class SomeBeanDy {
+    private String field1;
+    private String field2;
+    private String field3;
+
+    public SomeBeanDy(String field1, String field2, String field3) {
+        this.field1 = field1;
+        this.field2 = field2;
+        this.field3 = field3;
+    }
+}
+
