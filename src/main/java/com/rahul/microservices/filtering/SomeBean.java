1b85db23b3e02d8011a1891a7f5938372f254cbe
RAHUL <cycv@jhhj/com>
2024-10-20 22:45:46 +0530

    Filtering 1) Static Filtering 2) Dynamic Filtering

---
+++ src/main/java/com/rahul/microservices/filtering/SomeBean.java
@@ -0,0 +1,29 @@
+package com.rahul.microservices.filtering;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+//static filtering
+
+@Getter
+@Setter
+@ToString
+public class SomeBean {
+    private String field1;
+    @JsonIgnore                       //on browser when API hit, it wont show the filed2
+    private String field2;
+    private String field3;
+
+    public SomeBean(String field1, String field2, String field3) {
+        this.field1 = field1;
+        this.field2 = field2;
+        this.field3 = field3;
+    }
+}
+
+//   @JsonIgnoreProperties("field2","field3")                        //you can also ignore at class level
+//   public class SomeBean {
+
+//best way to use only @JsonIgnore above variables
