286e8a041d08672d8a5348c6bb92bc1c9e24e08c
RAHUL <cycv@jhhj/com>
2024-10-20 18:30:15 +0530

    Version REST API 3=>Custom header versioning 4=> Content Negotiation Versioning

--- src/main/java/com/rahul/microservices/versioning/VersioningPersonController.java
+++ src/main/java/com/rahul/microservices/versioning/VersioningPersonController.java
@@ -6,7 +6,8 @@ import org.springframework.web.bind.annotation.RestController;
 //Version REST API
 //1=> URI versioning
 //2=> Request Parameter versioning
-
+//3=>Custom header versioning
+//4=> Content Negotiation Versioning
 @RestController
 public class VersioningPersonController {
 
@@ -28,6 +29,28 @@ public class VersioningPersonController {
 
     @GetMapping(path = "/person",params = "version=2")
     public Personv2 getSecondRequestParameterVersion(){
+        return new Personv2("Rahul","Mulik");
+    }
+
+    //3=>Custom header versioning
+    @GetMapping(path = "/person/header", headers = "X-API-VERSION=1")                 ///localhost:8080/person/header   //in psotmane choose header, key as X-API-VERSION and value 1
+    public Person getFirstCustomeHeaderVersion(){
+        return new Person("Rahul Mulik");
+    }
+
+    @GetMapping(path = "/person/header",headers = "X-API-VERSION=2")
+    public Personv2 getSecondCustomeHeaderVersion(){
+        return new Personv2("Rahul","Mulik");
+    }
+
+    //Content Negotiation Versioning
+    @GetMapping(path = "/person/accept", produces = "application/vnd.company.app-v1+json")              ///localhost:8080/person/header   //in psotmane choose header, key as accept and value application/vnd.company.app-v1+json
+    public Person getFirstVersionOfPersonAcceptHeader() {
+        return new Person("Rahul Mulik");
+    }
+
+    @GetMapping(path = "/person/accept", produces = "application/vnd.company.app-v2+json")
+    public Personv2 getSecondVersionOfPersonAcceptHeader() {
         return new Personv2("Rahul","Mulik");
     }
 }
