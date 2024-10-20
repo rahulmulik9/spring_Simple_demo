2ff375c109f25619b7b1f1c09c1cdf8eb0520cb9
RAHUL <cycv@jhhj/com>
2024-10-20 16:22:02 +0530

    Added messages.properties

--- src/main/java/com/rahul/microservices/helloWorld/HelloWorldController.java
+++ src/main/java/com/rahul/microservices/helloWorld/HelloWorldController.java
@@ -1,10 +1,20 @@
 package com.rahul.microservices.helloWorld;
 
+import org.springframework.cglib.core.Local;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Locale;
 
 @RestController
 public class HelloWorldController {
 
+    private MessageSource messageSource;
+
+    public HelloWorldController(MessageSource theMessageSource){
+        this.messageSource = theMessageSource;
+    }
     //old way mapping
     @RequestMapping(method =  RequestMethod.GET,path = "/hi")
     public String hello(){return "Hello World";}
@@ -19,6 +29,11 @@ public class HelloWorldController {
     public HelloWordBean hellowithName(@PathVariable String theName){
        // return new HelloWordBean("Hi "+theName);
         return new HelloWordBean(String.format("Hello , %s", theName));
-
     }
+
+    @RequestMapping(method =  RequestMethod.GET,path = "/hiInter")
+    public String helloInternationlise(){
+        Locale local = LocaleContextHolder.getLocale();
+        return  messageSource.getMessage("good.morning.message", null,"Default Message",local);
+     }
 }
